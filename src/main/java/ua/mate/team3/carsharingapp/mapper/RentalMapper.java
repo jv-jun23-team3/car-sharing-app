82ef1fa09bf661c5949bbd91b2882ff5e8105255
Arsen Mehdalskyi <arsen25public@gmail.com>
2023-10-02 18:11:46 +0300

    Fixed checkstyle

--- src/main/java/ua/mate/team3/carsharingapp/mapper/RentalMapper.java
+++ src/main/java/ua/mate/team3/carsharingapp/mapper/RentalMapper.java
@@ -1,7 +1,8 @@
 package ua.mate.team3.carsharingapp.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.MapperConfig;
+import org.mapstruct.Mapping;
+import ua.mate.team3.carsharingapp.config.MapperConfig;
 import ua.mate.team3.carsharingapp.dto.rental.CreateRentalRequestDto;
 import ua.mate.team3.carsharingapp.dto.rental.ResponseRentalDto;
 import ua.mate.team3.carsharingapp.model.Rental;
@@ -10,5 +11,7 @@ import ua.mate.team3.carsharingapp.model.Rental;
 public interface RentalMapper {
     ResponseRentalDto toResponseDto(Rental rental);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "deleted", ignore = true)
     Rental toEntity(CreateRentalRequestDto requestDto);
 }
--- src/main/java/ua/mate/team3/carsharingapp/service/RentalService.java
+++ src/main/java/ua/mate/team3/carsharingapp/service/RentalService.java
@@ -1,11 +1,10 @@
 package ua.mate.team3.carsharingapp.service;
 
-import ua.mate.team3.carsharingapp.dto.rental.CreateRentalRequestDto;
-import ua.mate.team3.carsharingapp.dto.rental.ResponseRentalDto;
-
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
+import ua.mate.team3.carsharingapp.dto.rental.CreateRentalRequestDto;
+import ua.mate.team3.carsharingapp.dto.rental.ResponseRentalDto;
 
 public interface RentalService {
     ResponseRentalDto save(CreateRentalRequestDto requestDto);
