8128e9d91c5a9fdef6fddb86b826b5c7230eb1f9
Roman Kuchmii <romakuchmii@gmail.com>
2023-10-02 22:53:13 +0300

    [PAYMENT] added payment model and repository

---
+++ src/main/java/ua/mate/team3/carsharingapp/enums/Type.java
@@ -0,0 +1,5 @@
+package ua.mate.team3.carsharingapp.enums;
+
+public enum Type {
+    PAYMENT, FINE
+}
---
+++ src/main/java/ua/mate/team3/carsharingapp/model/Payment.java
@@ -0,0 +1,47 @@
+package ua.mate.team3.carsharingapp.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import ua.mate.team3.carsharingapp.enums.Status;
+import ua.mate.team3.carsharingapp.enums.Type;
+import lombok.Data;
+
+@Data
+@Entity
+@SQLDelete(sql = "UPDATE payments SET is_deleted = true WHERE id = ?")
+@Where(clause = "is_deleted = false")
+@Table(name = "payments")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
+    private Type type;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "rental_id", nullable = false)
+    private Rental rentalId;
+    @Column(name = "session_url", nullable = false)
+    private String sessionUrl;
+    @Column(name = "session_id", nullable = false)
+    private String sessionId;
+    @Column(nullable = false)
+    private BigDecimal amount;
+    @Column(name = "is_deleted")
+    private boolean isDeleted;
+}
