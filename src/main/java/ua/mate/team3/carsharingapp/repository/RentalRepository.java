a1b946dc428ae85e81194cd76f43adbb48390724
Arsen Mehdalskyi <arsen25public@gmail.com>
2023-10-02 17:57:23 +0300

    Created a skeleton for rental management

--- src/main/java/ua/mate/team3/carsharingapp/repository/RentalRepository.java
+++ src/main/java/ua/mate/team3/carsharingapp/repository/RentalRepository.java
@@ -2,8 +2,8 @@ package ua.mate.team3.carsharingapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ua.mate.team3.carsharingapp.model.Car;
+import ua.mate.team3.carsharingapp.model.Rental;
 
 @Repository
-public interface CarRepository extends JpaRepository<Car, Long> {
+public interface RentalRepository extends JpaRepository<Rental, Long> {
 }
