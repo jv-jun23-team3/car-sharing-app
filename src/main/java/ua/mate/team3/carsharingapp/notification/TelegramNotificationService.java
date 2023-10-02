07e467d409a8c9d81a49cc0d409466396cdf129f
Arsen Mehdalskyi <arsen25public@gmail.com>
2023-10-02 16:29:22 +0300

    Good

--- src/main/java/ua/mate/team3/carsharingapp/notification/TelegramNotificationService.java
+++ src/main/java/ua/mate/team3/carsharingapp/notification/TelegramNotificationService.java
@@ -1,16 +1,15 @@
 package ua.mate.team3.carsharingapp.notification;
 
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+@Service
 public class TelegramNotificationService extends TelegramLongPollingBot implements NotificationService {
-    public TelegramNotificationService(String botToken) {
-        super(botToken);
-    }
-
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage()) {
@@ -27,11 +26,6 @@ public class TelegramNotificationService extends TelegramLongPollingBot implemen
         }
     }
 
-    @Override
-    public String getBotUsername() {
-        return "car_sharing_project_bot";
-    }
-
     public void sendNotification(String chatId, String message) {
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(chatId);
@@ -42,5 +36,15 @@ public class TelegramNotificationService extends TelegramLongPollingBot implemen
         } catch (TelegramApiException e) {
             throw new RuntimeException("Could not send message");
         }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "car_sharing_project_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "{telegram.token}";
     }
 }
