97db068889592c8a6cb2cd5a72db027022d3e2a0
Arsen Mehdalskyi <arsen25public@gmail.com>
2023-10-02 17:07:02 +0300

    Created NotificationException

---
+++ src/main/java/ua/mate/team3/carsharingapp/exception/NotificationException.java
@@ -0,0 +1,7 @@
+package ua.mate.team3.carsharingapp.exception;
+
+public class NotificationException extends RuntimeException {
+    public NotificationException(String message) {
+        super(message);
+    }
+}
