10387a0560464d470106f2ce9de24770b0b95c4d
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-10-02 11:43:31 +0300

    init commit

---
+++ src/test/java/ua/mate/team3/carsharingapp/CarSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.mate.team3.carsharingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
