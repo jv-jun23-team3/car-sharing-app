5e777454d35a4ebf70a75fdd76f7779c75c64c9e
Arsen Mehdalskyi <arsen25public@gmail.com>
2023-10-03 12:03:23 +0300

    Improved NotificationService

--- src/main/java/ua/mate/team3/carsharingapp/exception/NotificationException.java
+++ src/main/java/ua/mate/team3/carsharingapp/exception/NotificationException.java
@@ -4,4 +4,8 @@ public class NotificationException extends RuntimeException {
     public NotificationException(String message) {
         super(message);
     }
+
+    public NotificationException(String message, Throwable e) {
+        super(message, e);
+    }
 }
--- src/main/java/ua/mate/team3/carsharingapp/notification/NotificationService.java
+++
@@ -1,7 +0,0 @@
-package ua.mate.team3.carsharingapp.notification;
-
-import org.telegram.telegrambots.meta.generics.LongPollingBot;
-
-public interface NotificationService extends LongPollingBot {
-
-}
