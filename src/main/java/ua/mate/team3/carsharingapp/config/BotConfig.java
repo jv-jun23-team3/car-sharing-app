802ef71451d5e38f0fa6e8b968207b543a7e1fdc
Arsen Mehdalskyi <arsen25public@gmail.com>
2023-10-03 14:16:15 +0300

    fixed checkstyle

---
+++ src/main/java/ua/mate/team3/carsharingapp/config/BotConfig.java
@@ -0,0 +1,35 @@
+package ua.mate.team3.carsharingapp.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import ua.mate.team3.carsharingapp.bots.TelegramBot;
+import ua.mate.team3.carsharingapp.exception.NotificationException;
+
+@Configuration
+public class BotConfig {
+    @Value(value = "${telegram.token}")
+    private String botToken;
+    @Value(value = "${telegram.bot.name}")
+    private String botUserName;
+
+    @Bean
+    public TelegramBot telegramBot() {
+        return new TelegramBot(botToken, botUserName);
+    }
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() {
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(telegramBot());
+        } catch (TelegramApiException e) {
+            throw new NotificationException("Can't initialize bot", e);
+        }
+    }
+}
