86f0b4049ffea78a4e51555bbf6c99921994fc16
Arsen Mehdalskyi <arsen25public@gmail.com>
2023-10-03 18:34:17 +0300

    changed ManyToOne to OneToOne

--- src/main/java/ua/mate/team3/carsharingapp/model/Rental.java
+++ src/main/java/ua/mate/team3/carsharingapp/model/Rental.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
@@ -36,7 +37,7 @@ public class Rental {
     private LocalDateTime actualReturnDate;
 
     @NotNull
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "car_id", nullable = false)
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
