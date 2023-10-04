fdd452ccb8830a9dc93d19397de66dc45368272d
Arsenii Mehdalskyi <114875417+Zel1oy@users.noreply.github.com>
2023-10-04 16:28:30 +0300

    fixed bots architecture (#29)
    Co-authored-by: Arsen Mehdalskyi <arsen25public@gmail.com>

--- src/main/java/ua/mate/team3/carsharingapp/service/impl/TelegramNotificationService.java
+++ src/main/java/ua/mate/team3/carsharingapp/service/impl/TelegramNotificationService.java
@@ -1,5 +1,6 @@
 package ua.mate.team3.carsharingapp.service.impl;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ua.mate.team3.carsharingapp.bots.TelegramBot;
@@ -8,28 +9,14 @@ import ua.mate.team3.carsharingapp.service.NotificationService;
 @Service
 @RequiredArgsConstructor
 public class TelegramNotificationService implements NotificationService {
-    private final TelegramBot arsenTelegramBot;
-    private final TelegramBot bogdanTelegramBot;
-    private final TelegramBot romaTelegramBot;
-    private final TelegramBot vitaliyTelegramBot;
-    private final TelegramBot olegTelegramBot;
+    private final List<TelegramBot> botList;
 
     @Override
     public void sendNotification(String message) {
-        if (arsenTelegramBot.getChatId() != null) {
-            arsenTelegramBot.sendMessage(arsenTelegramBot.getChatId(), message);
-        }
-        if (bogdanTelegramBot.getChatId() != null) {
-            bogdanTelegramBot.sendMessage(bogdanTelegramBot.getChatId(), message);
-        }
-        if (romaTelegramBot.getChatId() != null) {
-            romaTelegramBot.sendMessage(bogdanTelegramBot.getChatId(), message);
-        }
-        if (vitaliyTelegramBot.getChatId() != null) {
-            vitaliyTelegramBot.sendMessage(vitaliyTelegramBot.getChatId(), message);
-        }
-        if (olegTelegramBot.getChatId() != null) {
-            olegTelegramBot.sendMessage(olegTelegramBot.getChatId(), message);
+        for (TelegramBot adminBot : botList) {
+            if (adminBot.getChatId() != null) {
+                adminBot.sendMessage(adminBot.getChatId(), message);
+            }
         }
     }
 }
