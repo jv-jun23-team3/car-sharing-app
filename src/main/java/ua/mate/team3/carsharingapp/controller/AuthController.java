dc07084f04ce9abe353f0d8437dae65953799592
Vitalii Kukoba <126148357+SaiyaJ1N@users.noreply.github.com>
2023-10-04 15:43:20 +0300

    added documentation to AuthenticationController (#26)
    Co-authored-by: Vitalii <mkukoba11@ukr.net>

--- src/main/java/ua/mate/team3/carsharingapp/controller/AuthController.java
+++ src/main/java/ua/mate/team3/carsharingapp/controller/AuthController.java
@@ -1,5 +1,6 @@
 package ua.mate.team3.carsharingapp.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +24,8 @@ public class AuthController {
     private final UserService userService;
     private final AuthenticationService authenticationService;
 
+    @Operation(summary = "Login of user", description = "Validate user`s login and password, "
+            + "if all input parameters is valid, return created JWT Token to current user.")
     @PostMapping("/register")
     public UserRegistrationResponseDto register(
             @RequestBody @Valid UserRegistrationRequestDto registrationRequest)
@@ -30,6 +33,9 @@ public class AuthController {
         return userService.register(registrationRequest);
     }
 
+    @Operation(summary = "Registration of user", description = "Checks user input parameters"
+            + " and validate them, if email does not exist in data base,"
+            + " and password and repeat password are equals, saves the user to db.")
     @PostMapping("/login")
     public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto loginRequestDto) {
         return authenticationService.authenticate(loginRequestDto);
