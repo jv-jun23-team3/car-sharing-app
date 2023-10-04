ec87408a8e3204b82fe862298a7a895c555d1585
Arsen Mehdalskyi <arsen25public@gmail.com>
2023-10-04 17:14:04 +0300

    pre check

---
+++ src/test/java/ua/mate/team3/carsharingapp/config/SqlFilePaths.java
@@ -0,0 +1,5 @@
+package ua.mate.team3.carsharingapp.config;
+
+public class SqlFilePaths {
+    public static final String USER_ROLE_INSERT = "classpath:db/user-role-insert.sql";
+}
---
+++ src/test/java/ua/mate/team3/carsharingapp/user/repository/UserRepositoryTest.java
@@ -0,0 +1,56 @@
+package ua.mate.team3.carsharingapp.user.repository;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Collections;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+import ua.mate.team3.carsharingapp.model.Role;
+import ua.mate.team3.carsharingapp.model.User;
+import ua.mate.team3.carsharingapp.repository.UserRepository;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class UserRepositoryTest {
+    private static User expectedUser;
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeAll
+    static void beforeAll() {
+        expectedUser = new User();
+        expectedUser.setId(3L);
+        expectedUser.setFirstName("UserName 1");
+        expectedUser.setLastName("LastName 1");
+        expectedUser.setPassword("Password 1");
+        expectedUser.setEmail("email@gmail.com");
+        Role role = new Role();
+        role.setName(Role.RoleName.ROLE_CUSTOMER);
+        role.setId(1L);
+        expectedUser.setRoles(Collections.singleton(role));
+    }
+
+    @Sql(scripts = USER_ROLE_INSERT, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = USER_ROLE_DELETE, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @Test
+    public void findByEmail_validEmail_ReturnsUser() {
+        User user = userRepository.findByEmail("email@gmail.com").orElseThrow();
+        assertEquals(expectedUser,user);
+    }
+
+    @Sql(scripts = USER_ROLE_INSERT, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = USER_ROLE_DELETE, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @Test
+    public void findByEmail_nonValidEmail_ThrowsException() {
+        String expectedMessage = "Can't find user with email: nonValidEmail";
+        EntityNotFoundException exception = assertThrows(EntityNotFoundException.class,
+                () -> userRepository.findByEmail("nonValidEmail")
+                        .orElseThrow(() -> new EntityNotFoundException(expectedMessage)));
+        assertEquals(expectedMessage, exception.getMessage());
+    }
+}
