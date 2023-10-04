cbc59d6fc86f2eb20af5ae12086b67fb6abebe13
Roman Kuchmii <romakuchmii@gmail.com>
2023-10-04 16:00:03 +0300

    removed redundant code

---
+++ src/test/java/ua/mate/team3/carsharingapp/PaymentRepositoryTest.java
@@ -0,0 +1,8 @@
+package ua.mate.team3.carsharingapp;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class PaymentRepositoryTest {
+}
