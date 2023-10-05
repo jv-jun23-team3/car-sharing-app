89f5240f0ba9f8d08e513ea5b635cae642268e10
Bohdan Kaziamyr <113326347+padu12@users.noreply.github.com>
2023-10-05 11:45:19 +0300

    add /error to requestMatchers (#39)

--- src/main/java/ua/mate/team3/carsharingapp/config/SecurityConfig.java
+++ src/main/java/ua/mate/team3/carsharingapp/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -38,11 +39,13 @@ public class SecurityConfig {
                 .authorizeHttpRequests(
                         auth -> auth
                                 .requestMatchers("/auth/**",
-                                        "/payments/success", "/payments/cancel")
+                                        "/payments/success", "/payments/cancel",
+                                        "/error")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
                 )
+                .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthenticationFilter,
