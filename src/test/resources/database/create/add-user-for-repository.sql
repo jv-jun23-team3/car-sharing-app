dcf90ba1499417769386e48c392427664470cdcc
Bohdan Kaziamyr <113326347+padu12@users.noreply.github.com>
2023-10-05 23:56:39 +0300

    Revert "[RENTAL_TESTS] Rental tests" (#52)

--- src/test/resources/database/create/add-user-for-repository.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users (id, email, password, first_name, last_name, is_deleted) VALUES (1, 'customer@example.com', 'password', 'Customer', 'User', false);
-INSERT INTO users (id, email, password, first_name, last_name, is_deleted) VALUES (2, 'manager@example.com', 'password', 'Manager', 'Admin', false);
-
-INSERT INTO user_role (user_id, role_id) VALUES (1, 1);
-INSERT INTO user_role (user_id, role_id) VALUES (2, 2);
