c01f71a5a1ba6042b68fdeacf043c5f5657bfc04
Arsenii Mehdalskyi <114875417+Zel1oy@users.noreply.github.com>
2023-10-10 23:46:12 +0300

    deleted all bots, beside of one and optimized sending notifications for many users through 1 bot (#56)
    Co-authored-by: Arsen Mehdalskyi <arsen25public@gmail.com>

--- src/main/java/ua/mate/team3/carsharingapp/service/impl/TelegramNotificationService.java
+++ src/main/java/ua/mate/team3/carsharingapp/service/impl/TelegramNotificationService.java
@@ -1,6 +1,5 @@
 package ua.mate.team3.carsharingapp.service.impl;
 
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ua.mate.team3.carsharingapp.bots.TelegramBot;
@@ -9,14 +8,12 @@ import ua.mate.team3.carsharingapp.service.NotificationService;
 @Service
 @RequiredArgsConstructor
 public class TelegramNotificationService implements NotificationService {
-    private final List<TelegramBot> botList;
+    private final TelegramBot notificationBot;
 
     @Override
     public void sendNotification(String message) {
-        for (TelegramBot adminBot : botList) {
-            if (adminBot.getChatId() != null) {
-                adminBot.sendMessage(adminBot.getChatId(), message);
-            }
+        for (Long chatId : notificationBot.getChatIds()) {
+            notificationBot.sendMessage(chatId, message);
         }
     }
 }
